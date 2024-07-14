4b6b49f80a428308784d722ab47f5bfbd44083e5
Tatyana Sender <tatsiana.sender@gmail.com>
2024-07-14 12:25:45 +0200

    feat: searchbar to fc

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Component } from 'react';
+import { FC, ChangeEvent } from 'react';
 import styles from './SearchBar.module.css';
 
 interface IProps {
@@ -7,25 +7,15 @@ interface IProps {
   onSearch: () => void;
 }
 
-interface IState {}
-
-class SearchBar extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  render() {
-    const { searchTerm, onSearchInputChange, onSearch } = this.props;
-
-    return (
-      <div className={styles.wrapper}>
-        <input type="text" value={searchTerm} onChange={onSearchInputChange} placeholder="Enter search term..." />
-        <button className={styles.btn} onClick={onSearch}>
-          Search
-        </button>
-      </div>
-    );
-  }
-}
+const SearchBar: FC<IProps> = ({ searchTerm, onSearchInputChange, onSearch }) => {
+  return (
+    <div className={styles.wrapper}>
+      <input type="text" value={searchTerm} onChange={onSearchInputChange} placeholder="Enter search term..." />
+      <button className={styles.btn} onClick={onSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
