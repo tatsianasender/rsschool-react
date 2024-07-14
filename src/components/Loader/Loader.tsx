60129023dd32166919f8dde7327365c16f91b162
Tatyana Sender <tatsiana.sender@gmail.com>
2024-07-14 12:25:18 +0200

    feat: loader to fc

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,13 +1,7 @@
-import { Component } from 'react';
 import styles from './Loader.module.css';
 
-interface IProps {}
-interface IState {}
-
-class Loader extends Component<IProps, IState> {
-  render() {
-    return <div className={styles.loader}></div>;
-  }
-}
+const Loader = () => {
+  return <div className={styles.loader}></div>;
+};
 
 export default Loader;
