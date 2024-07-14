bd1ad738627dfc6a3e4432de547b9b4d45420b12
Tatyana Sender <tatsiana.sender@gmail.com>
2024-07-14 12:26:09 +0200

    feat: searchresults to fc

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Person } from '../../types/types';
 import styles from './SearchResults.module.css';
 
@@ -6,33 +6,24 @@ interface IProps {
   searchResults: Person[];
 }
 
-interface IState {}
-
-class SearchResults extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-  }
-  render() {
-    const { searchResults } = this.props;
-
-    return (
-      <div className={styles.wrapper}>
-        {searchResults.length > 0 ? (
-          searchResults.map((result, index) => (
-            <div key={index} className={styles.card}>
-              <h4>{result.name}</h4>
-              <div>
-                <strong>height: </strong>
-                {result.height}
-              </div>
+const SearchResults: FC<IProps> = ({ searchResults }) => {
+  return (
+    <div className={styles.wrapper}>
+      {searchResults.length > 0 ? (
+        searchResults.map((result, index) => (
+          <div key={index} className={styles.card}>
+            <h4>{result.name}</h4>
+            <div>
+              <strong>height: </strong>
+              {result.height}
             </div>
-          ))
-        ) : (
-          <h2>Nothing found.</h2>
-        )}
-      </div>
-    );
-  }
-}
+          </div>
+        ))
+      ) : (
+        <h2>Nothing found.</h2>
+      )}
+    </div>
+  );
+};
 
 export default SearchResults;
